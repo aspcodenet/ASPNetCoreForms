d2e36b2f9d6e45c53c42d0272732a368d706081d
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-07-07 13:42:27 +0200

    a

--- ASPNetCoreEFCoreFacit/Data/Utbildning.cs
+++ ASPNetCoreEFCoreFacit/Data/Utbildning.cs
@@ -1,9 +1,13 @@
-﻿namespace ASPNetCoreEFCoreFacit.Data
+﻿using System.Collections.Generic;
+
+namespace ASPNetCoreEFCoreFacit.Data
 {
     public class Utbildning
     {
         public int Id { get; set; }
         
         public string Namn { get; set; }
+
+        public List<CV> Cvs { get; set; }
     }
 }

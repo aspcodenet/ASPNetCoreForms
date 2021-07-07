552166194a3fce8b194d141092bdd4229c0e7450
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-07-07 10:38:33 +0200

    Add project files.

---
+++ ASPNetCoreEFCoreFacit/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ASPNetCoreEFCoreFacit.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        :base(options)
+        {
+            
+        }
+
+        public DbSet<Bil> Bilar { get; set; }
+        public DbSet<Lastbil> Lastbilar { get; set; }
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+    }
+}
---
+++ ASPNetCoreEFCoreFacit/Data/Bil.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPNetCoreEFCoreFacit.Data
+{
+    public class Bil
+    {
+        public int Id { get; set; }
+
+        public Manufacturer Manufacturer { get; set; }
+
+        [MaxLength(20)]
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+
+        public bool HasRadio { get; set; }
+
+        [MaxLength(6)]
+        public string RegNo { get; set; }
+    }
+}
